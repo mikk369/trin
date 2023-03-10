791bf6125107b9ed271ed7ffd656276eabd34e12
mikk369 <mikk.ereline@vikk.ee>
2023-03-10 12:42:58 +0200

    added fontawesome icons

--- src/main.js
+++ src/main.js
@@ -10,13 +10,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import specific icons */
-import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { faTable } from '@fortawesome/free-solid-svg-icons';
-import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
-library.add(faCircleQuestion, faFile, faTable, faImage);
+library.add(fas);
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
